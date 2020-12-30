8ca05c576c54082b773db851871bb83617e5affb
richard <zfy1045056938@gmail.com>
2020-12-30 04:28:56 -0800

    1.rnd enemies

--- 1229/Scripts/Network/NetworkStartPositionForClass.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-public class NetworkStartPositionForClass : MonoBehaviour {
-    public Players playerPrefab;
-
-}
--- 1229/Scripts/UI/Players/InventorySlot.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Script.UI.Players
-{
-    public class InventorySlot : MonoBehaviour
-    {
-        
-    }
-}
